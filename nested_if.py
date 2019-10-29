1c7c736b77ecb5e9158c0e42df5aa0371841c8c0
confoundedkyle <1125125225184+confoundedkyle@users.noreply.github.com>
2019-10-29 07:53:48 -0600

    Added print(tax) to code to show an output

--- nested_if.py
+++ nested_if.py
@@ -2,11 +2,12 @@ country = input("What country do you live in? ")
 
 if country.lower() == 'Canada':
     province = input("What province/state do you live in? ")
-    if province in('Alberta','Nunavut','Yukon'):
+    if province.lower() in('Alberta','Nunavut','Yukon'):
         tax = 0.05
-    elif province == 'Ontario':
+    elif province.lower() == 'Ontario':
         tax = 0.13
     else:
         tax = 0.15
 else:
     tax = 0.0
+print(tax)
