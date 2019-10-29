86dccc6b44737d6a3d25893a2a25f9d88815fec7
confoundedkyle <1125125225184+confoundedkyle@users.noreply.github.com>
2019-10-29 07:13:02 -0600

    Added or statement to clean code

--- multiple_if_statements.py
+++ multiple_if_statements.py
@@ -1,10 +1,8 @@
 province = input("what province do yo ulive in ")
 tax = 0
 
-if province == 'Alberta':
-    tax = 0.05
-elif province == 'Nanavut':
-    tax = 0.05
+if province == 'Alberta' or province == 'Nanavut':
+    tax = 0.05 
 elif province == 'Ontario':
     tax = 0.13
 else: 
