88ab6e227b0bbc1d634f0a52805ae94a62146754
confoundedkyle <27352794+confoundedkyle@users.noreply.github.com>
2019-11-18 22:24:07 -0700

    Loops Initial Commit

---
+++ 27_loops_conditional_loops.py
@@ -0,0 +1,10 @@
+# https://www.youtube.com/watch?v=LrOAl8vUFHY
+
+# https://github.com/microsoft/c9-python-getting-started/tree/master/12%20-%20Loops
+
+names = ['Christopher', 'Susan']
+index = 0
+while index < len(names):
+    print(names[index])
+    # Change the condition!!
+    index = index + 1
---
+++ 27_loops_numbers_index.py
@@ -0,0 +1,12 @@
+# https://www.youtube.com/watch?v=LrOAl8vUFHY
+
+# https://github.com/microsoft/c9-python-getting-started/blob/master/12%20-%20Loops/number.py
+
+
+# range creates an array
+# First parameter is the starter
+# Second indicates the number of numbers to create
+# range(0, 5) creates [0, 1, 2, 3, 4]
+
+for index in range(0, 5):
+	print(index)
